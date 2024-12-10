6cacccd1d861a30cfdae2da00b759f2f4997deb8
Roger Randle <randleroger44@gmail.com>
2024-12-10 16:59:26 -0500

    Change backend port to 5001

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import Todo from './models/Todo.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
