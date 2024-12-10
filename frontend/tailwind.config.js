c2a1fd5833264cd32fe67ff98874117dc3911871
Roger Randle <randleroger44@gmail.com>
2024-12-10 16:54:13 -0500

    Fixed tailwind.config.js and updated build scripts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import scrollbar from 'tailwind-scrollbar';
+
 export const content = [
   "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   "./components/**/*.{js,ts,jsx,tsx,mdx}",
   "./app/**/*.{js,ts,jsx,tsx,mdx}",
 ];
+
 export const theme = {
   extend: {},
 };
-export const plugins = [require('tailwind-scrollbar')];
+
+export const plugins = [scrollbar];
